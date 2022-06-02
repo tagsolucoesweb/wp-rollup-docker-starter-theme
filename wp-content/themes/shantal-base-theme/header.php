b1a5bbe3ecc347222668dc6bce0560a51b15b120
gustavo-cvly <gustavo@convincely.com>
2022-06-02 09:28:42 -0300

    fix(header): page title bug

--- wp-content/themes/shantal-base-theme/header.php
+++ wp-content/themes/shantal-base-theme/header.php
@@ -4,7 +4,6 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Shantal Base</title>
     <?php wp_head();?>
 </head>
 <body <?php body_class(); ?>>
